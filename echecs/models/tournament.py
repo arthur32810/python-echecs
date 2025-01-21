65264588d50d84032c6d5e231828d1ba18f0196d
arthur <arthur32810@gmail.com>
2025-01-21 17:36:22 +0100

    ajout fonction to_dict et from_dict à Tournament

--- echecs/models/tournament.py
+++ echecs/models/tournament.py
@@ -7,3 +7,21 @@ class Tournament:
         self.end_date = end_date
         self.round = round
         self.note = note
+
+    def to_dict(self):
+        """Convertit un objet Tournament en dictionnaire pour JSON"""
+        return {
+            "name": self.name,
+            "place": self.place,
+            "start_date": self.start_date,
+            "end_date": self.end_date,
+            "round": self.round,
+            "note": self.note,
+        }
+
+    @staticmethod
+    def from_dict(data):
+        """Recrée un objet Tournament à partir d'un dictionnaire"""
+        return Tournament(
+            data["name"], data["place"], data["start_date"], data["end_date"], data["round"], data["note"]
+        )
