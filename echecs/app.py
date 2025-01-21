f8074b8548f58c60153d835586c5642058b40c68
arthur <arthur32810@gmail.com>
2025-01-21 14:41:04 +0100

    format code

--- echecs/app.py
+++ echecs/app.py
@@ -1,9 +1,9 @@
+import pdb
+import subprocess as sp
+
 from echecs.controllers.home_controller import HomeController
 from echecs.controllers.player_controller import PlayerController
 from echecs.models.store import Store
-
-import subprocess as sp
-import pdb
 
 
 class Application:
